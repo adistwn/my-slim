e55a843a3f73b1d64a8cf37f69af610bdbdccb58
adistwn <miminukm@gmail.com>
2020-01-24 17:03:23 +0700

    add valitron/validator

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,19 @@
 
 namespace Routes;
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
 use App\Users;
+use Valitron\Validator;
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Psr\Http\Message\ServerRequestInterface as Request;
 
 class Web
 {
     public function routes()
     {
-        $app = new \Slim\App();
+        $config = [
+            'displayErrorDetails' => true,
+        ];
+        $app = new \Slim\App($config);
 
         /**
          * Default route
@@ -32,7 +36,27 @@ class Web
         $app->post('/store', function (Request $request, Response $response) {
             
             $input = $request->getParsedBody();
-            return $response->write($input['name']);
+
+            $v = new Validator($input);
+            $v->rule('required', 'name');
+
+            if ($v->validate()) {
+                
+                $result = [
+                    'error'   => false,
+                    'message' => 'Created',
+                    'data'    => []
+                ];
+
+            } else {
+
+                $result = [
+                    'error'   => false,
+                    'message' => $v->errors(),
+                    'data'    => []
+                ];
+            }
+            return $response->withJson($result);
         });
         
 
