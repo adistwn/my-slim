371586ef9bbc697cd10abf24e59b4600b52edc8d
adistwn <miminukm@gmail.com>
2020-01-28 15:35:13 +0700

    jwt class

---
+++ app/controllers/Jwt.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Handle JWT
+ * 
+ * @author AdiStwn
+ */
+
+namespace App\Controllers;
+
+use \Lcobucci\JWT\Parser;
+use \Lcobucci\JWT\Builder;
+use \Lcobucci\JWT\Signer\Key;
+use \Lcobucci\JWT\ValidationData;
+
+class Jwt
+{
+    /**
+     * Generate token JWT
+     *
+     * @package \Lcobucci\JWT\
+     * @param Int $uid
+     * @return mixed $token
+     */
+    public function generateToken(Int $uid)
+    {
+        $time = time();
+        $token = (new Builder())->issuedBy('localhost/my-slim') // Configures the issuer (iss claim)
+            ->permittedFor('localhost') // Configures the audience (aud claim)
+            ->identifiedBy('4f1g23a12aa', true) // Configures the id (jti claim), replicating as a header item
+            ->issuedAt($time) // Configures the time that the token was issue (iat claim)
+            ->canOnlyBeUsedAfter($time) // Configures the time that the token can be used (nbf claim)
+            ->expiresAt($time + 86400) // Configures the expiration time of the token (exp claim)
+            ->withClaim('uid', $uid) // Configures a new claim, called "uid"
+            ->getToken(); // Retrieves the generated token
+
+        $token->getHeaders(); // Retrieves the token headers
+        $token->getClaims(); // Retrieves the token claims
+        return $token;
+    }
+
+    /**
+     * Validating token JWT
+     *
+     * @param String $token
+     * @return bool
+     */
+    public function validateToken(String $token)
+    {
+        $validToken = (new Parser())->parse((string) $token);
+
+        $data = new ValidationData(); // It will use the current time to validate (iat, nbf and exp)
+        $data->setIssuer('localhost/my-slim');
+        $data->setAudience('localhost');
+        $data->setId('4f1g23a12aa');
+
+        if ($validToken->validate($data) === false) {
+            return false;
+        }
+
+        return true;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,7 @@ namespace Routes;
 
 use App\User;
 use \Valitron\Validator;
-use \Lcobucci\JWT\Parser;
-use \Lcobucci\JWT\Builder;
-use \Lcobucci\JWT\Signer\Key;
-use \Lcobucci\JWT\ValidationData;
+use App\Controllers\Jwt;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -136,7 +133,7 @@ class Web
 
                     if (password_verify($input['password'], $user->password)) {
 
-                        $tokenJwt = (string) Web::generateToken($user->id);
+                        $tokenJwt = (string) Jwt::generateToken($user->id);
 
                         $user->token = $tokenJwt;
                         $user->save();
@@ -183,49 +180,4 @@ class Web
         return $app->run();
     }
 
-    /**
-     * Generate token JWT
-     *
-     * @package \Lcobucci\JWT\
-     * @param Int $uid
-     * @return mixed $token
-     */
-    public function generateToken(Int $uid)
-    {
-        $time = time();
-        $token = (new Builder())->issuedBy('localhost/my-slim') // Configures the issuer (iss claim)
-            ->permittedFor('localhost') // Configures the audience (aud claim)
-            ->identifiedBy('4f1g23a12aa', true) // Configures the id (jti claim), replicating as a header item
-            ->issuedAt($time) // Configures the time that the token was issue (iat claim)
-            ->canOnlyBeUsedAfter($time) // Configures the time that the token can be used (nbf claim)
-            ->expiresAt($time + 86400) // Configures the expiration time of the token (exp claim)
-            ->withClaim('uid', $uid) // Configures a new claim, called "uid"
-            ->getToken(); // Retrieves the generated token
-
-        $token->getHeaders(); // Retrieves the token headers
-        $token->getClaims(); // Retrieves the token claims
-        return $token;
-    }
-
-    /**
-     * Validating token JWT
-     *
-     * @param String $token
-     * @return bool
-     */
-    public function validateToken(String $token)
-    {
-        $validToken = (new Parser())->parse((string) $token);
-
-        $data = new ValidationData(); // It will use the current time to validate (iat, nbf and exp)
-        $data->setIssuer('localhost/my-slim');
-        $data->setAudience('localhost');
-        $data->setId('4f1g23a12aa');
-
-        if ($validToken->validate($data) === false) {
-            return false;
-        }
-
-        return true;
-    }
 }
